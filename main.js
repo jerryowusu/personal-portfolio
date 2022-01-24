ef6cd5da30cba23143c7fc18142d915a110d85ff
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-24 14:35:19 +0000

    added a js file and link to html

---
+++ main.js
@@ -0,0 +1,5 @@
+const html = document.getElementsByTagName('html')[0];
+const navMenu = document.querySelector('.nav-menu');
+const showBtn = document.querySelector('nav .show-menu-btn');
+const closedBtn = document.querySelector('nav .closed-menu-btn');
+const navlinks = document.querySelectorAll('nav ul li');
