22d7a2d005ed1c17636b633354d0b58f0ceb3950
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-24 14:35:37 +0000

    added variables

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const navMenu = document.querySelector('.nav-menu');
 const showBtn = document.querySelector('nav .show-menu-btn');
 const closedBtn = document.querySelector('nav .closed-menu-btn');
 const navlinks = document.querySelectorAll('nav ul li');
+
+
