7dd6c3421dd9b0205b1105cf1109ddbf667ebad5
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-25 15:12:00 -0800

    Add try-popup-window

---
+++ js/project.js
@@ -0,0 +1,41 @@
+const Project = {
+  project1: {
+    "name": "Tonic",
+    "img": "../images/nature.png",
+    "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "technologies": ["Html", "Ruby on Rails", "Javascript"],
+    "seelive": "jerryowusu.github.io/personal-portfolio",
+    "source": "github.com/jerryowusu"
+  },
+  project2: {
+    "name": "Tonic",
+    "img": "/images/nature.png",
+    "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "technologies": ["Html", "Ruby on Rails", "Javascript"],
+    "seelive": "jerryowusu.github.io/personal-portfolio",
+    "source": "github.com/jerryowusu"
+  },
+  project3: {
+    "name": "Tonic",
+    "img": "/images/nature.png",
+    "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "technologies": ["Html", "Ruby on Rails", "Javascript"],
+    "seelive": "jerryowusu.github.io/personal-portfolio",
+    "source": "github.com/jerryowusu"
+  },
+  project4: {
+    "name": "Tonic",
+    "img": ("images/nature.png"),
+    "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "technologies": ["Html", "Ruby on Rails", "Javascript"],
+    "seelive": "jerryowusu.github.io/personal-portfolio",
+    "source": "github.com/jerryowusu"
+  }
+}
+
+console.log(Project.project1.name)
+console.log(Project.project4.img)
+console.log(Project.project1.technologies[1])
+
+
+
