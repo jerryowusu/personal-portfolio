6240a045c240ae4ef83d71c743d8fc90bed0beb0
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-26 11:38:44 +0000

    Changed images from png to svg

--- js/project.js
+++ js/project.js
@@ -2,7 +2,7 @@ const works = [
   {
     name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    imageURL: '../images/project1-snapshot.png',
+    imageURL: '../images/project1-snapshot.svg',
     technologies: ['html', 'css', 'javaScript'],
     liveURL: '#',
     sourceURL: 'https://github.com/jerryowusu/',
@@ -10,7 +10,7 @@ const works = [
   {
     name: 'Uber Navigation',
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    imageURL: '../images/project2-snapshot.png',
+    imageURL: '../images/project2-snapshot.svg',
     technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
     liveURL: '#',
     sourceURL: 'https://github.com/jerryowusu/',      
@@ -18,7 +18,7 @@ const works = [
   {
     name: 'Facebook 360',
     description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    imageURL: '../images/project3-snapshot.png',
+    imageURL: '../images/project3-snapshot.svg',
     technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
     liveURL: '#',
     sourceURL: 'https://github.com/jerryowusu/',
@@ -26,7 +26,7 @@ const works = [
   {
     name: 'Multi-Post Stories',
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    imageURL: '../images/project4-snapshot.png',
+    imageURL: '../images/project4-snapshot.svg',
     technologies: ['html', 'css', 'javaScript'],
     liveURL: '#',
     sourceURL: 'https://github.com/jerryowusu/',
