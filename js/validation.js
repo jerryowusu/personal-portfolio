8509109482061ed520b6386b82466d4b2f14da1a
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-27 10:21:32 +0000

    Update email validaiton

--- js/validation.js
+++ js/validation.js
@@ -1,17 +1,21 @@
 const form = document.querySelector('.contact-form');
-const errorMessage = document.querySelector('.validateEmail');
+const errorMessage = document.querySelector('.error-message');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); const { email } = form.elements;
+  event.preventDefault()
+  let email = document.querySelector('.email');
 
-  if (email.value === email.value.toLowerCase()) {
-    form.submit();
+  if (email.value !== email.value.toLowerCase()) {
+    errorMessage.classList.add('email-error');
+    errorMessage.innerText = 'Please enter the email in small letters';
+    setTimeout(() => { 
+        errorMessage.classList.remove('email-error');
+        errorMessage.innerText = '';
+     }, 5000);
+    event.preventDefault();
   }
 
   else {
-    errorMessage.classList.add('email-error');
-    errorMessage.innerText = 'Please enter the email in lowercase';
-    setTimeout(() => { errorMessage.classList.add('email-error'); }, 8000);
-    event.preventDefault();
+    form.submit()
   }
 });
