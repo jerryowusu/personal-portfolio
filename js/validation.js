f6ec90be71b9a94f53a588888dc25c4ca5c642f0
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-27 10:34:04 +0000

    Fix linter error

--- js/validation.js
+++ js/validation.js
@@ -6,14 +6,14 @@ form.addEventListener('submit', (event) => {
   const email = document.querySelector('.email');
 
   if (email.value !== email.value.toLowerCase()) {
-      errorMessage.classList.add('email-error');
-      errorMessage.innerText = 'Please enter the email in small letters';
+    errorMessage.classList.add('email-error');
+    errorMessage.innerText = 'Please enter the email in small letters';
     setTimeout(() => {
       errorMessage.classList.remove('email-error');
       errorMessage.innerText = '';
     }, 5000);
     event.preventDefault();
-   }
+}
 
   else {
     form.submit();
