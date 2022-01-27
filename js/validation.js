1bfa728ca90fe8c4411188780f6177254fd5b11e
Cynthia Inga <candyinga67@gmail.com>
2022-01-27 11:28:40 +0200

    Add form condition of validation form

--- js/validation.js
+++ js/validation.js
@@ -1,2 +1,17 @@
 const form = document.querySelector('.contact-form');
-const errorMessage = document.querySelector('.validateEmail')
+const errorMessage = document.querySelector('.validateEmail');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); const { email } = form.elements;
+
+  if (email.value === email.value.toLowerCase()) {
+    form.submit();
+  }
+
+  else {
+    errorMessage.classList.add('email-error');
+    errorMessage.innerText = 'Please enter the email in lowercase';
+    setTimeout(() => { errorMessage.classList.add('email-error'); }, 8000);
+    event.preventDefault();
+  }
+});
