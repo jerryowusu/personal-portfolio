a0438b36f9e16c8e8af0fe685ede95a5ece5d789
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-27 10:30:51 +0000

    Fix linter errors

--- js/validation.js
+++ js/validation.js
@@ -2,20 +2,20 @@ const form = document.querySelector('.contact-form');
 const errorMessage = document.querySelector('.error-message');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  let email = document.querySelector('.email');
+  event.preventDefault();
+  const email = document.querySelector('.email');
 
   if (email.value !== email.value.toLowerCase()) {
-    errorMessage.classList.add('email-error');
-    errorMessage.innerText = 'Please enter the email in small letters';
-    setTimeout(() => { 
-        errorMessage.classList.remove('email-error');
-        errorMessage.innerText = '';
-     }, 5000);
+      errorMessage.classList.add('email-error');
+      errorMessage.innerText = 'Please enter the email in small letters';
+    setTimeout(() => {
+      errorMessage.classList.remove('email-error');
+      errorMessage.innerText = '';
+    }, 5000);
     event.preventDefault();
-  }
+   }
 
   else {
-    form.submit()
+    form.submit();
   }
 });
