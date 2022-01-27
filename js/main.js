c639bbea3adfb53b514b87c3d0184f8c4f5fbc88
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-27 13:04:01 +0000

    Add input data into localstorage

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ linksInMobileMenu.forEach((link) => {
     mobileMenu.style.display = 'none';
   });
 });
+
+
+const button = document.querySelector('form .button');
+
+const localStorageData = () => {
+  const formData = {
+    name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    message: document.querySelector('#message').value
+  }
+
+  localStorage.setItem('formData', JSON.stringify(formData))
+}
+
+button.addEventListener('click', localStorageData)
